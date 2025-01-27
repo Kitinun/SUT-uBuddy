f770208f0f33bd3e62e32e150117d989d289476d
Kitinun <kitinun.khonson@gmail.com>
2025-01-27 15:24:19 +0700

    fix: 🐛 Delete Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./page/Login";
 import Mainpage from "./page/MainPage";
-import ProtectedRoute from "./components/Layout/ProtectedRoute";
-import AuthLayout from "./components/Layout/AuthLayout";
+import Navbar from "./components/Layout/Navbar";
 
 function App() {
-  // ฟังก์ชันสำหรับเช็คว่ามี authentication หรือไม่
-  const isAuthenticated = () => {
-    const token = localStorage.getItem("token");
-    const user = localStorage.getItem("user");
-    return token && user;
-  };
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            isAuthenticated() ? <Navigate to="/mainpage" replace /> : <Login />
-          }
-        />
+        <Route path="/" element={<Login />} />
 
         <Route
           path="/mainpage"
           element={
-            <ProtectedRoute>
-              <AuthLayout>
-                <Mainpage />
-              </AuthLayout>
-            </ProtectedRoute>
+            <>
+              <Navbar />
+              <Mainpage />
+            </>
           }
         />
       </Routes>
