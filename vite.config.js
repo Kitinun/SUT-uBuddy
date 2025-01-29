0acfa69c95c93736f9512320a2782a132b7b48e0
Kitinun <kitinun.khonson@gmail.com>
2025-01-29 15:37:18 +0700

    fix: 🐛 Fix config env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+// สร้างฟังก์ชันสำหรับตรวจสอบ environment
+const getBase = (mode) => {
+  if (mode === "production") {
+    return "/SutUbuddy/";
+  }
+  return "/";
+};
+
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/SutUbuddy/",
-});
+  base: getBase(mode),
+}));
